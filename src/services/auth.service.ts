6a2846a218c072e9493cd6a9a02d133ea416e1e1
rizwanulwahid612 <rizwanulwahid612@gmail.com>
2023-11-20 01:33:03 +0600

    tyghf

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -28,7 +28,7 @@ export const removeUserInfo = (key: string) => {
 };
 export const getNewAccessToken = async () => {
   return await axiosInstance({
-    url: `http://localhost:3005/api/v1/auth/refresh-token`,
+    url: "http://localhost:3005/api/v1/auth/refresh-token",
     method: "POST",
     headers: { "Content-Type": "application/json" },
     withCredentials: true,
